f49ea005dcafa33609363aefd4cb35c93e04b7cf
maustin1 <mtv1243@gmail.com>
2021-01-26 17:18:49 -0500

    change airtable embed visual syle

--- src/components/airtableEmbed/AirTableEmbed.js
+++ src/components/airtableEmbed/AirTableEmbed.js
@@ -3,7 +3,7 @@ import './airtableEmbed.scss';
 function AirTableEmbed() {
     return (
         <section id="airtableEmbed" className="airtableEmbed">
-            <iframe className="airtable-embed" src="https://airtable.com/embed/shrcjsZQIHecEQdXf?backgroundColor=red&layout=card&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533"></iframe>
+            <iframe class="airtable-embed" src="https://airtable.com/embed/shrcjsZQIHecEQdXf?backgroundColor=red&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533"></iframe>
         </section>
     )
 }
